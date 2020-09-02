ecb02ac384c5dda200f5db8e63166fa6dbe1a6e8
Valentin Olchowski <valentin.olchowski@gmail.com>
2020-09-02 09:27:00 +0200

    init

---
+++ src/main/java/org/acme/ExampleService.java
@@ -0,0 +1,10 @@
+package org.acme;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ExampleService {
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+}
